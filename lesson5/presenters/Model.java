e3b1da5e0547b9455e722a5505d11e077f35966c
AndreiParmon <118477491+AndreiParmon@users.noreply.github.com>
2023-05-03 20:59:10 +0300

    add Model

---
+++ lesson5/presenters/Model.java
@@ -0,0 +1,13 @@
+package lesson5.presenters;
+
+import lesson5.models.Table;
+
+import java.util.Collection;
+import java.util.Date;
+
+public interface Model {
+    Collection<Table> loadTables();
+
+    int reservationTable(Date reservationDate, int tableNo, String name);
+    int changeReservationTable(int oldReservation, Date newDateReservation, int tableNo, String name);
+}
