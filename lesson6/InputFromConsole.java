8e1de310a5bcb0a3c2166a8544f6dd7af1db7c11
AndreiParmon <118477491+AndreiParmon@users.noreply.github.com>
2023-05-21 16:02:07 +0300

    Create InputFromConsole.java

---
+++ lesson6/InputFromConsole.java
@@ -0,0 +1,25 @@
+package lesson6.srp2;
+
+import java.util.Scanner;
+
+public class InputFromConsole {
+
+    private Order order;
+
+    public InputFromConsole(Order order) {
+        this.order = order;
+    }
+
+    public void inputFromConsole(){
+        order.setClientName(prompt("Client name: "));
+        order.setProduct(prompt("Product: "));
+        order.setQnt(Integer.parseInt(prompt("Quantity: ")));
+        order.setPrice(Integer.parseInt(prompt("Price: ")));
+    }
+
+    private String prompt(String message){
+        Scanner in = new Scanner(System.in);
+        System.out.print(message);
+        return in.nextLine();
+    }
+}
