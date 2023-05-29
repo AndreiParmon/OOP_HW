9b8c5d227cecc5fd72637e63a40630a8d2f9653e
AndreiParmon <118477491+AndreiParmon@users.noreply.github.com>
2023-05-29 20:28:25 +0300

    Create Senior.java

---
+++ lesson7/Senior.java
@@ -0,0 +1,32 @@
+package lesson7.observer;
+
+public class Senior implements Observer {
+    private String name;
+    private double salary;
+    private JobType jobType;
+
+    public Senior(String name) {
+        this.name = name;
+        salary = 80000;
+        jobType = JobType.SENIOR;
+    }
+
+
+    @Override
+    public void newReceiveOffer(Vacancy vacancy) {
+        if (this.salary < salary){
+            System.out.printf("Старший пециалист %s >>> Мне нужна эта работа! [%s - %f]\n",
+                    name, vacancy.getNameCompany(), vacancy.getSalary());
+            this.salary = vacancy.getSalary();
+        }
+        else{
+            System.out.printf("Старший пециалист %s >>> Я найду работу получше! [%s - %f]\n",
+                    name, vacancy.getNameCompany(), vacancy.getSalary());
+        }
+    }
+
+    @Override
+    public String getJobType() {
+        return jobType.getJobType();
+    }
+}
